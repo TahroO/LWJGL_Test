def007c71f58b1599865b9b7893c713ca43de60a
Bernd <bernd.reusch@hotmail.com>
2023-03-19 17:06:37 +0100

    issue

---
+++ src/main/resources/tiled/test1/Run-Sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Run-Sheet" tilewidth="80" tileheight="80" tilecount="8" columns="8">
+ <image source="../../Downloads/Legacy-Fantasy - High Forest 2.0/Legacy-Fantasy - High Forest 2.3/Character/Run/Run-Sheet.png" width="640" height="80"/>
+</tileset>
---
+++ src/main/resources/tiled/test1/Walk-Base-Sheet-Boar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Walk-Base-Sheet" tilewidth="48" tileheight="32" tilecount="6" columns="6">
+ <image source="../../Downloads/Legacy-Fantasy - High Forest 2.0/Legacy-Fantasy - High Forest 2.3/Mob/Boar/Walk/Walk-Base-Sheet.png" width="288" height="32"/>
+</tileset>
