a86e55a9c3bfd8ef42579f51b6944baf415af575
Bernd <bernd.reusch@hotmail.com>
2023-03-20 21:35:16 +0100

    h

--- src/main/resources/tiled/king-pigs/BombPig.tsx
+++ src/main/resources/tiled/king-pigs/BombPig.tsx
@@ -1,4 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="BombPig" tilewidth="26" tileheight="26" tilecount="10" columns="10">
  <image source="05-Pig Thowing a Bomb/Idle (26x26).png" width="260" height="26"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="9" duration="200"/>
+  </animation>
+ </tile>
 </tileset>
