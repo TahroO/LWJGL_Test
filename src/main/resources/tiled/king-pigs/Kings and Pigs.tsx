a0f3c90f11ab73ab9c9fe7d7a2fcff8d506d3c87
Bernd <bernd.reusch@hotmail.com>
2023-03-20 21:29:48 +0100

    murks

---
+++ src/main/resources/tiled/king-pigs/Kings and Pigs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Kings and Pigs" tilewidth="134" tileheight="14" tilecount="1" columns="1">
+ <image source="king-pigs/Kings and Pigs.png" width="134" height="14"/>
+</tileset>
