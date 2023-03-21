8c8979ea61fd563dd9e9bd7ceef9cd80c7fcb8ef
Steffen <steffen@13-bits.de>
2023-03-21 21:09:44 +0100

    x

--- src/main/java/brot/lwjgl/engine/testing/scenes/TiledMapScene.java
+++ src/main/java/brot/lwjgl/engine/testing/scenes/TiledMapScene.java
@@ -1,11 +1,14 @@
 package brot.lwjgl.engine.testing.scenes;
 
+import brot.lwjgl.engine.MouseInput;
 import brot.lwjgl.engine.Window;
+import brot.lwjgl.engine.scene.Camera;
 import brot.lwjgl.engine.scene.Entity;
 import brot.lwjgl.engine.scene.SceneLayer;
 import brot.lwjgl.engine.scene.Scene;
 import brot.lwjgl.engine.tiled.*;
 import brot.lwjgl.engine.util.XmlLoader;
+import org.joml.Vector2f;
 import org.joml.Vector3f;
 
 import java.util.*;
@@ -14,6 +17,9 @@ import static org.lwjgl.glfw.GLFW.*;
 
 public class TiledMapScene {
     Entity player;
+    private static final float MOUSE_SENSITIVITY = 40f;
+    private static final float MOVEMENT_SPEED = .3f;
+
 
     public void init(Scene scene) {
         TiledMap map = XmlLoader.loadTiledXml(TiledMap.class, "mossy.tmx");
@@ -44,14 +50,38 @@ public class TiledMapScene {
     }
 
     public void input(Window window, Scene scene, long diffTimeMillis) {
-        if (window.isKeyPressed(GLFW_KEY_A)) {
-            player.getPosition().x -= 1.8;
-            player.setOrientationX(-1);
-        } else if (window.isKeyPressed(GLFW_KEY_D)) {
-            player.getPosition().x += 1.8;
-            player.setOrientationX(1);
+        float move = Math.round(diffTimeMillis * MOVEMENT_SPEED);
+        Camera camera = scene.getCamera();
+        if (window.isKeyPressed(GLFW_KEY_W)) {
+            camera.moveUp(move);
+        } else if (window.isKeyPressed(GLFW_KEY_S)) {
+            camera.moveDown(move);
         }
-        player.updateModelMatrix();
+        if (window.isKeyPressed(GLFW_KEY_A)) {
+            camera.moveLeft(move);
+        } else if (window.isKeyPressed(GLFW_KEY_D)) {
+            camera.moveRight(move);
+        }
+
+        MouseInput mouseInput = window.getMouseInput();
+        if (mouseInput.isRightButtonPressed()) {
+            Vector2f displVec = mouseInput.getDisplVec();
+            camera.moveUp(Math.round(Math.toRadians(-displVec.x * MOUSE_SENSITIVITY)));
+            camera.moveRight(Math.round(Math.toRadians(-displVec.y * MOUSE_SENSITIVITY)));
+        }
+
+        Vector3f cameraPosition = camera.getPosition();
+        float camY = Math.min(Math.max(cameraPosition.y, 0), scene.getHeight() - scene.getViewportHeight());
+        float camX = Math.min(Math.max(cameraPosition.x, 0), scene.getWidth() - scene.getViewportWidth());
+        camera.setPosition(camX, camY);
+//        if (window.isKeyPressed(GLFW_KEY_A)) {
+//            player.getPosition().x -= 1.8;
+//            player.setOrientationX(-1);
+//        } else if (window.isKeyPressed(GLFW_KEY_D)) {
+//            player.getPosition().x += 1.8;
+//            player.setOrientationX(1);
+//        }
+//        player.updateModelMatrix();
 
     }
 
